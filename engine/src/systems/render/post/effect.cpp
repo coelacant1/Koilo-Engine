56ec501df9cd42dd4e4d5ea23bdbb8a294881032
Coela Can't <coelacannot@gmail.com>
2026-03-08 01:44:35 -0500

    Rename PTX -> Koilo file structure

--- engine/src/systems/render/post/effect.cpp
+++
@@ -1,5 +0,0 @@
-#include <ptx/systems/render/post/effect.hpp>
-
-void Effect::SetRatio(float r) {
-    ratio = Mathematics::Constrain(r, 0.0f, 1.0f);
-}
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "../lib/ptx/core/math/vector3d.hpp" 
-
-void setup() {
-  Vector3D myVector(1.0f, 2.0f, 3.0f);
-}
-
-void loop() {
-  
-}
